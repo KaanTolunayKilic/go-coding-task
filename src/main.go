1c6e159e05fb0478a9af644cb55665523e59395d
Kaan Tolunay Kilic <kaankilic@MacBook-Pro.local>
2020-06-22 15:37:15 +0200

    chore: add helper function for error handling

--- src/main.go
+++ src/main.go
@@ -4,30 +4,44 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"strings"
+	"os"
 )
 
 func main() {
-	userIDPtr := flag.Int("userId", -1, "User id")
+	userIDPtr := flag.Int("userId", -1, "User id *required")
 	filterPtr := flag.String("filter", "", "Filter comments")
 	flag.Parse()
 
-	client := NewClient(nil)
-
-	posts, err := client.UserPosts(*userIDPtr)
-	if err != nil {
-		log.Fatalf("List posts failed with error: %v", err)
+	if *userIDPtr == -1 {
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
 
+	client := NewClient(nil)
 	fmt.Printf("Posts and comments for user with id %d:\n", *userIDPtr)
-	for _, post := range posts {
+	for _, post := range client.posts(*userIDPtr) {
 		PrintPost(post)
-		comments, _ := client.PostComments(post.ID)
-		for _, comment := range comments {
-			if strings.Contains(comment.Body, *filterPtr) {
+		for _, comment := range client.comments(&post) {
+			if comment.contains(*filterPtr) {
 				PrintComment(comment)
 			}
 		}
 		PrintDivider()
 	}
+}
+
+func (c *Client) comments(p *Post) []Comment {
+	comments, err := c.PostComments(p.ID)
+	if err != nil {
+		log.Fatalf("Could not request comments for post with id %d: %s", p.ID, err)
+	}
+	return comments
+}
+
+func (c *Client) posts(userID int) []Post {
+	posts, err := c.UserPosts(userID)
+	if err != nil {
+		log.Fatalf("Could not request posts for user with id %d: %s", userID, err)
+	}
+	return posts
 }
